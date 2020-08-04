69199a0b6870e149b8424bb0858097ba38934559
shiv raj <shiv1988@gmail.com>
2020-08-04 19:45:06 -0400

    workoutlog api updated

--- api/awards.js
+++ api/awards.js
@@ -19,14 +19,10 @@ router.post('/week',
     async (req, res) => {
         try {
             const { userId } = req.body;
-            console.log(userId);
             let start = new Date(moment().subtract(7, 'days').startOf('day'));
-            console.log('Start ', start);
             let end = new Date(moment().subtract(1, 'days').endOf('day'));
-            console.log('End ', end);
             const bestSet = await db.getDb().collection('bestsets')
                 .find({ userId, date: { $gte: start, $lt: end } }).sort({ date: -1 }).toArray();
-            console.log(bestSet);
             res.json(bestSet);
         }
         catch (err) {
