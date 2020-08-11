2010d6006dd22e472ea4b57ceca2398fa9caf190
shiv raj <shiv1988@gmail.com>
2020-08-10 20:12:10 -0400

    homeworkoutlog api

--- api/homeworkoutlog.js
+++ api/homeworkoutlog.js
@@ -4,22 +4,19 @@ const db = require('../config/db');
 const { ObjectId } = require('mongodb');
 const moment = require('moment');
 
-//@route POST api/workoutlog
+//@route POST api/homeworkoutlog
 //@desc Insert workoutlog for a particular exercise
 router.post('/',
     [
         check('name', 'Name is required').not().isEmpty(),
-        check('date', 'Date is required').not().isEmpty(),
-        check('weight', 'Weight is required').not().isEmpty(),
-        check('unit', 'Unit is required').not().isEmpty(),
-        check('count', 'Count is required').not().isEmpty()
+        check('date', 'Date is required').not().isEmpty()
     ],
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        const { userId, category, name, date, weight, unit, count } = req.body;
+        const { userId, category, name, date, weight, count, time } = req.body;
         try {
             let workoutlog = {
                 'userId': userId,
@@ -27,15 +24,11 @@ router.post('/',
                 'name': name,
                 'date': new Date(date),
                 'weight': weight,
-                'unit': unit,
-                'count': count
+                'count': count,
+                'time': time
             };
-            let log;
-            let woCategory = category.toLowerCase();
-            log = await db.getDb().collection(`${woCategory}workoutlogs`).insertOne(workoutlog);
-            workoutlog.logId = log.insertedId
-            await db.getDb().collection('workoutlogs').insertOne(workoutlog);
-            res.send('Workoutlog added!');
+            await db.getDb().collection('homeworkoutlogs').insertOne(workoutlog);
+            res.send('HomeWorkoutlog added!');
         }
         catch (err) {
             console.log(err);
@@ -43,13 +36,13 @@ router.post('/',
     }
 )
 
-//@route GET api/workoutlog/:category/:name
+//@route GET api/homeworkoutlog/:category/:name
 //@desc Get logs for an exercise
 router.post('/log',
     async (req, res) => {
         try {
             const { userId, category, name } = req.body;
-            let exerciselogs = await db.getDb().collection(`${category.toLowerCase()}workoutlogs`)
+            let exerciselogs = await db.getDb().collection(`homeworkoutlogs`)
                 .find({ userId: userId, name: name }).sort({ date: -1 }).toArray();
             res.json(exerciselogs);
         }
@@ -59,34 +52,7 @@ router.post('/log',
     }
 )
 
-//@route GET api/workoutlog/logs
-//@desc Get logs of all exercises
-router.post('/logs',
-    async (req, res) => {
-        try {
-            const { userId } = req.body;
-            let exercise = {};
-            let logs = await db.getDb().collection('workoutlogs').find({ userId: userId }).sort({ date: -1 }).toArray();
-            for (let log of logs) {
-                if (exercise[log.name]) {
-                    log.maxWeight = exercise[log.name];
-                }
-                else {
-                    const maxWt = await db.getDb().collection('maxweights').findOne({ userId: userId, name: log.name });
-                    exercise[log.name] = maxWt.weight;
-                    log.maxWeight = exercise[log.name];
-                    log.maxWeightCount = maxWt.count;
-                }
-            }
-            res.json(logs);
-        }
-        catch (err) {
-            console.log(err);
-        }
-    }
-);
-
-//@route GET api/workoutlog/logsWeek
+//@route GET api/homeworkoutlog/logsWeek
 //@desc Get logs of all exercises for a week
 router.post('/logsWeek',
     async (req, res) => {
@@ -94,7 +60,7 @@ router.post('/logsWeek',
             const { userId } = req.body;
             let start = new Date(moment().subtract(7, 'days').startOf('day'));
             let end = new Date(moment().subtract(1, 'days').endOf('day'));
-            let logs = await db.getDb().collection('workoutlogs')
+            let logs = await db.getDb().collection('homeworkoutlogs')
                 .find({ userId, date: { $gte: start, $lt: end } }).sort({ date: -1 }).toArray();
             res.json(logs);
         }
@@ -104,13 +70,13 @@ router.post('/logsWeek',
     }
 );
 
-//@route PUT api/workoutlog/note
+//@route PUT api/homeworkoutlog/note
 //@desc Update note for a log
 router.put('/note',
     async (req, res) => {
         try {
             const { id, category, note } = req.body;
-            let updateNote = await db.getDb().collection(`${category.toLowerCase()}workoutlogs`)
+            let updateNote = await db.getDb().collection(`homeworkoutlogs`)
                 .updateOne({ "_id": ObjectId(id) }, { $set: { "note": note } });
             res.json(updateNote);
         }
