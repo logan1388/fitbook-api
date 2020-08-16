1f2ca20197ab2773191f5bfbfaf4fd15a8957b20
shiv raj <shiv1988@gmail.com>
2020-08-16 09:34:55 -0400

    Added homeworkouts to highlights

--- api/awards.js
+++ api/awards.js
@@ -19,11 +19,19 @@ router.post('/week',
     async (req, res) => {
         try {
             const { userId } = req.body;
-            let start = new Date(moment().subtract(7, 'days').startOf('day'));
+            let start = new Date(moment().subtract(20, 'days').startOf('day'));
             let end = new Date(moment().subtract(1, 'days').endOf('day'));
+            const awards = [];
             const bestSet = await db.getDb().collection('bestsets')
                 .find({ userId, date: { $gte: start, $lt: end } }).sort({ date: -1 }).toArray();
-            res.json(bestSet);
+            bestSet.forEach(set => awards.push(set));
+            const maxReps = await db.getDb().collection('maxreps')
+                .find({ userId, date: { $gte: start, $lt: end } }).sort({ date: -1 }).toArray();
+            maxReps.forEach(rep => rep.category === 'home' && awards.push(rep));
+            // const maxTime = await db.getDb().collection('maxtime')
+            //     .find({ userId, date: { $gte: start, $lt: end } }).sort({ date: -1 }).toArray();
+            // maxTime.forEach(time => awards.push(time));
+            res.json(awards);
         }
         catch (err) {
             console.log(err);
